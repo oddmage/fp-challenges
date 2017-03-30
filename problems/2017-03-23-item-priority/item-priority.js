318fd7338ca9e06eb4e2f4df8dc50ed048512aa3
oddmage <oddmage@gmail.com>
2017-03-30 17:34:28 -0400

    Add files via upload
    Hopefully this just goes to my fork.

--- problems/2017-03-23-item-priority/item-priority.js
+++ problems/2017-03-23-item-priority/item-priority.js
@@ -1,3 +1,17 @@
+const R = require('ramda');
+
 exports.default = function(items, categories) {
-  return null;
-}
+  const findFirstListing = function(category) {
+      return R.find(function (movie) {
+          return R.propEq('category', category)(movie);
+      })(items);
+  };
+
+  const findFirstUsedCategory = R.find(findFirstListing);
+
+  const decideTheUndecided = function(movie) {
+      return movie || items[0];
+  };
+
+  return R.compose(decideTheUndecided, findFirstListing, findFirstUsedCategory)(categories);
+};
